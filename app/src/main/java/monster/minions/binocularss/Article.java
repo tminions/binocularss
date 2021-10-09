2af2c430b5252404173595c1fce621e3d4d20fbf
hisbaan <hisbaan@gmail.com>
2021-10-09 18:40:46 -0400

    Add Article class
    Co-authored-by: Eamon Ma <m@eamonma.com>
    Co-authored-by: Simon Chen <simonchen.sc.2002@gmail.com>
    Co-authored-by: Tai Zhang <tai.zhang2002@gmail.com>
    Co-authored-by: Benson Chou <benson.chou@mail.utoronto.ca>
    Co-authored-by: Macdeini Niu <thecoolmac@hotmail.com>
    Co-authored-by: Ismail Ahmed <isahmed0149@gmail.com>

---
+++ app/src/main/java/monster/minions/binocularss/Article.java
@@ -0,0 +1,19 @@
+package monster.minions.binocularss;
+
+public class Article {
+    String title;
+    String date;
+    String source;
+    String author;
+    String text;
+    String description;
+
+    public Article(String title, String date, String source, String author, String text, String description) {
+        this.title = title;
+        this.date = date;
+        this.source = source;
+        this.author = author;
+        this.text = text;
+        this.description = description;
+    }
+}
