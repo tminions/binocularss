99fbf72085a3e68f3b051b4cf30aa94c4e121453
hisbaan <hisbaan@gmail.com>
2021-10-27 16:34:41 -0400

    Update Article and Feed objects

--- app/src/main/java/monster/minions/binocularss/Article.kt
+++ app/src/main/java/monster/minions/binocularss/Article.kt
@@ -8,16 +8,21 @@ import java.net.URL
 
 data class Article(
     var title: String = "", // The title of the item.
-    var link: String = "", // The URL of the item.
-    var description: String = "", // The item synopsis.
     var author: String = "", // Email address of the author of the item.
-    var category: String = "", // Includes the item in one or more categories.
-    var comments: String = "", // URL of a page for comments relating to the item.
-    var enclosure: String = "", // Describes a media object that is attached to the item.
-    var guid: String = "", // A string that uniquely identifies the item.
+    var link: String = "", // The URL of the item.
     var pubDate: String = "", // Indicates when the item was published.
+    var description: String = "", // The item synopsis.
+    var content: String = "", // The content of an item, also known as enclosure
+    var image: String = "", // An href to a header image
+    var audio: String = "", // An href to companion audio
+    var video: String = "", // An href to companion video
+    var sourceName: String = "", // The source name (name of the feed it came from)
+    var sourceUrl: String = "", // The source URL (url of the feed it came from)
+    var categories: MutableList<String> = mutableListOf<String>(), // A list of the appropriate categories (predefiend)
+    var guid: String = "", // A string that uniquely identifies the item.
     var source: String = "", // The RSS channel that the item came from.
 ) {
+
     override fun equals(other: Any?): Boolean {
         return when (other) {
             is Article -> {
--- app/src/main/java/monster/minions/binocularss/Feed.kt
+++ app/src/main/java/monster/minions/binocularss/Feed.kt
@@ -1,5 +1,7 @@
 package monster.minions.binocularss
 
+import com.prof.rssparser.Image
+
 /**
  * from https://validator.w3.org/feed/docs/rss2.html
  */
@@ -7,22 +9,10 @@ data class Feed(
     var title: String = "", // The name of the channel. It's how people refer to your service. If you have an HTML website that contains the same information as your RSS file, the title of your channel should be the same as the title of your website.
     var link: String = "", // The URL to the HTML website corresponding to the channel.
     var description: String = "", // Phrase or sentence describing the channel.
-    var language: String = "", // The language the channel is written in. This allows aggregators to group all Italian language sites, for example, on a single page.
-    var copyright: String = "", // Copyright notice for content in the channel.
-    var managingEditor: String = "", // Email address for person responsible for editorial content.
-    var webMaster: String = "", // Email address for person responsible for technical issues relating to channel.
-    var pubDate: String = "", // The publication date for the content in the channel. For example, the New York Times publishes on a daily basis, the publication date flips once every 24 hours. That's when the pubDate of the channel changes. All date-times in RSS conform to the Date and Time Specification of RFC 822, with the exception that the year may be expressed with two characters or four characters (four preferred).
     var lastBuildDate: String = "", // The last time the content of the channel changed.
-    var category: String = "", // Specify one or more categories that the channel belongs to. Follows the same rules as the <item>-level category element.
-    var generator: String = "", // A string indicating the program used to generate the channel.
-    var docs: String = "", // A URL that points to the documentation for the format used in the RSS file. It's probably a pointer to this page. It's for people who might stumble across an RSS file on a Web server 25 years from now and wonder what it is.
-    var cloud: String = "", // Allows processes to register with a cloud to be notified of updates to the channel, implementing a lightweight publish-subscribe protocol for RSS feeds.
-    var ttl: String = "", // ttl stands for time to live. It's a number of minutes that indicates how long a channel can be cached before refreshing from the source.
-    var image: String = "", // Specifies a GIF, JPEG or PNG image that can be displayed with the channel.
-    var textInput: String = "", // Specifies a text input box that can be displayed with the channel.
-    var skipHours: String = "", // A hint for aggregators telling them which hours they can skip.
-    var skipDays: String = "", // A hint for aggregators telling them which days they can skip.
-    var tags: List<String> = mutableListOf<String>(),
-    var priority: Int = 0,
-    var articles: MutableList<Article> = mutableListOf<Article>()
+    var image: Image? = null, // Specifies a GIF, JPEG or PNG image that can be displayed with the channel.
+    var updatePeriod: String = "", // Specifies the amount of time between updates for RSS aggregators.
+    var articles: MutableList<Article> = mutableListOf<Article>(), // A list of articles that the feed contains.
+    var tags: List<String> = mutableListOf<String>(), // The user assigned tags on a feed
+    var priority: Int = 0 // The computed priority score of a feed.
 )
