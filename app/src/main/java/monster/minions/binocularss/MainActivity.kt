5b3d3502b0d69390b4ea9d9d7dc2465e95fd2871
hisbaan <hisbaan@gmail.com>
2021-10-29 23:37:48 -0400

    Integrate Room datbase for persistent feed

--- app/src/main/java/monster/minions/binocularss/MainActivity.kt
+++ app/src/main/java/monster/minions/binocularss/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.*
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.prof.rssparser.Parser
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -25,6 +27,7 @@ import monster.minions.binocularss.ui.theme.BinoculaRSSTheme
 
 class MainActivity : ComponentActivity() {
 
+    // Variables that can be accessed from other classes by calling `MainActivity.<variable>`.
     companion object {
         // Global FeedGroup object
         var feedGroup: FeedGroup = FeedGroup()
@@ -37,18 +40,42 @@ class MainActivity : ComponentActivity() {
                 // .charset(Charset.forName("ISO-8859-7")) // Default is UTF-8
                 .cacheExpirationMillis(24L * 60L * 60L * 100L) // Set the cache to expire in one day
                 .build()
+
+
+        }
+        // Setup Room database
+        private lateinit var db: RoomDatabase
+        private lateinit var feedDao: FeedDao
+        private fun setDb(context: Context) {
+            db = Room.databaseBuilder(
+                context,
+                AppDatabase::class.java, "feed-db"
+            ).allowMainThreadQueries().build()
+            feedDao = (db as AppDatabase).feedDao()
         }
 
-        const val FEED_GROUP_KEY = "feedGroup"
+        // Key for accessing feed group in onSaveInstanceState
+        // const val FEED_GROUP_KEY = "feedGroup"
     }
 
+    // Local variables
+    private val feedGroupText = MutableStateFlow("Empty\n")
+
+    /**
+     * The function that is run when the activity is created. This is on app launch in this case.
+     * It is also called when the activity is destroyed then recreated. It initializes the main
+     * functionality of the application (UI, companion variables, etc.)
+     *
+     * @param savedInstanceState A bundle of parcelable information that was previously saved.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState != null) {
-            feedGroup = savedInstanceState.getParcelable<FeedGroup>("feedGroup")!!
-            Toast.makeText(this@MainActivity, feedGroup.feeds[0].title, Toast.LENGTH_SHORT).show()
-        }
+        // TODO this code goes with onSaveInstanceState and onRestoreInstanceState
+        // if (savedInstanceState != null) {
+        // feedGroup = savedInstanceState.getParcelable<FeedGroup>("feedGroup")!!
+        //    Toast.makeText(this@MainActivity, feedGroup.feeds[0].title, Toast.LENGTH_SHORT).show()
+        // }
 
         setContent {
             BinoculaRSSTheme {
@@ -59,24 +86,16 @@ class MainActivity : ComponentActivity() {
                         .fillMaxHeight(),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally) {
-                        FeedTitles(feedGroup)
+                        FeedTitles()
                         UpdateFeedButton()
                     }
                 }
             }
         }
 
-        if (feedGroup.feeds.isNullOrEmpty()) {
-            // Add some feeds to the feedGroup
-            feedGroup.feeds.add(Feed(link = "https://rss.cbc.ca/lineup/topstories.xml"))
-            feedGroup.feeds.add(Feed(link = "https://androidauthority.com/feed"))
-            feedGroup.feeds.add(Feed(link = "https://www.nasa.gov/rss/dyn/Gravity-Assist.rss"))
-            feedGroup.feeds.add(Feed(link = "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss"))
 
-            Toast.makeText(this@MainActivity, "Added Sample Feeds to feedGroup", Toast.LENGTH_SHORT)
-                .show()
-        }
-
+        // Set feed-db in companion object
+        setDb(applicationContext)
         // Set parser in companion object
         setParser(this@MainActivity)
     }
@@ -84,22 +103,20 @@ class MainActivity : ComponentActivity() {
     /**
      * Call the required functions to update the Rss feed.
      *
-     * @param feedGroup A group of feeds to be updated.
      * @param parser A parser with preconfigured settings.
-     * @param context The application context (current view).
      */
     @DelicateCoroutinesApi
-    private fun updateRss(feedGroup: FeedGroup, parser: Parser, context: Context) {
-        val viewModel = PullFeed(context)
+    private fun updateRss(parser: Parser) {
+        val viewModel = PullFeed()
 
         GlobalScope.launch(Dispatchers.Main) {
-            MainActivity.feedGroup = viewModel.pullRss(MainActivity.feedGroup, parser)
+            feedGroup = viewModel.pullRss(feedGroup, parser)
             // Debug toast to confirm that the data has come back to the MainActivity variable.
             // for (feed in MainActivity.feedGroup.feeds) {
             //     Toast.makeText(context, "Pulled: ${feed.title}", Toast.LENGTH_SHORT).show()
             // }
             var text = ""
-            for (feed in MainActivity.feedGroup.feeds) {
+            for (feed in feedGroup.feeds) {
                 text += feed.title
                 text += "\n"
             }
@@ -107,26 +124,89 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    // /**
+    //  * Saves instance state when activity is destroyed
+    //  *
+    //  * TODO: We do not need onSaveInstanceState (goes with onRestoreInstanceState) for the current
+    //  *  use case. The room database handles
+    //  *  it all. I am leaving it here so that whoever uses it has a base for the code
+    //  *
+    //  * @param outState A bundle of instance state information stored using key-value pairs
+    //  */
+    // override fun onSaveInstanceState(outState: Bundle) {
+    //     Log.d("MainActivity", "onSaveInstanceState called")
+    //     super.onSaveInstanceState(outState)
+    //     outState.putParcelable(FEED_GROUP_KEY, feedGroup)
+    // }
+
+    // /**
+    //  * Restores instance state when activity is recreated
+    //  *
+    //  * TODO: We do not need onRestoreInstanceState (goes with onSaveInstanceState) for the current
+    //  *  use case. The room database handles it all. I am leaving it here so that whoever uses it
+    //  *  has a base for the code
+    //  *
+    //  * @param savedInstanceState A bundle of instance state information stored using key-value pairs
+    //  */
+    // override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+    //     Log.d("MainActivity", "onRestoreInstanceState called")
+    //     super.onRestoreInstanceState(savedInstanceState)
+    //     feedGroup = savedInstanceState.getParcelable<FeedGroup>(FEED_GROUP_KEY)!!
+
+    //     var text = ""
+    //     for (feed in feedGroup.feeds) {
+    //         text += feed.title
+    //         text += "\n"
+    //     }
+    //     feedGroupText.value = text
+    // }
+
     /**
-     * Saves instance state when activity is destroyed
+     * Save the list of user feeds to the Room database (feed-db) for data persistence.
      *
-     * @param outState A bundle of instance state information stored using key-value pairs
+     * The database files can be found in `Android/data/data/monster.minions.binocularss.databases`.
+     *
+     * This function is called before `onStop` and `onDestroy` or any time a "pause" happens. This
+     * includes when an app is exited but not closed.
      */
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("MainActivity", "onSaveInstanceState called")
-        super.onSaveInstanceState(outState)
-        outState.putParcelable(FEED_GROUP_KEY, feedGroup)
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+        feedDao.insertAll(*(feedGroup.feeds.toTypedArray()))
     }
 
     /**
-     * Restores instance state when activity is recreated
+     * Gets the list of user feeds from the Room database (feed-db).
      *
-     * @param savedInstanceState A bundle of instance state information stored using key-value pairs
+     * The database files can be found in `Android/data/data/monster.minions.binocularss.databases`.
+     *
+     * This function is called after `onCreate` or any time a "resume" happens. This includes
+     * the app being opened after the app is exited but not closed.
      */
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        Log.d("MainActivity", "onRestoreInstanceState called")
-        super.onRestoreInstanceState(savedInstanceState)
-        feedGroup = savedInstanceState.getParcelable<FeedGroup>(FEED_GROUP_KEY)!!
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+
+        val feeds: MutableList<Feed> = feedDao.getAll()
+
+        feedGroup.feeds = feeds
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // NOT PERMANENT: If the user does not have any feeds added, add some.
+        if (feedGroup.feeds.isNullOrEmpty()) {
+            // Add some feeds to the feedGroup
+            feedGroup.feeds.add(Feed(link = "https://rss.cbc.ca/lineup/topstories.xml"))
+            feedGroup.feeds.add(Feed(link = "https://androidauthority.com/feed"))
+            // TODO This feed is malformed according to the exception that the xml parser throws.
+            //  We can use this to develop a bad formatting indication to the user
+            //  feedGroup.feeds.add(Feed(link = "https://www.nasa.gov/rss/dyn/Gravity-Assist.rss"))
+            feedGroup.feeds.add(Feed(link = "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss"))
+
+            // Tell the user that this change happened
+            Toast.makeText(this@MainActivity, "Added Sample Feeds to feedGroup", Toast.LENGTH_SHORT)
+                .show()
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
         var text = ""
         for (feed in feedGroup.feeds) {
@@ -136,17 +216,8 @@ class MainActivity : ComponentActivity() {
         feedGroupText.value = text
     }
 
-    /**
-     * TODO implement saving to some sort of DB
-     */
-    override fun onPause() {
-        super.onPause()
-    }
-
-    private val feedGroupText = MutableStateFlow("Empty\n")
-
     @Composable
-    fun FeedTitles(feedGroup: FeedGroup) {
+    fun FeedTitles() {
         val text by feedGroupText.collectAsState()
         Text(text = text)
     }
@@ -154,7 +225,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun UpdateFeedButton() {
         Button(
-            onClick = { updateRss(feedGroup, parser, this@MainActivity) }
+            onClick = { updateRss(parser) }
         ) {
             Text("Update RSS Feeds")
         }
@@ -165,7 +236,7 @@ class MainActivity : ComponentActivity() {
     fun DefaultPreview() {
         BinoculaRSSTheme {
             Column {
-                FeedTitles(MainActivity.feedGroup)
+                FeedTitles()
                 UpdateFeedButton()
             }
         }
