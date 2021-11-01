f426667d89683dbb980e818aed6793abc447330c
Ismail Ahmed <isahmed0149@gmail.com>
2021-11-01 15:45:13 -0400

    Moved BookmarksAcviity to activities package

---
+++ app/src/main/java/monster/minions/binocularss/activities/BookmarksActivity.kt
@@ -0,0 +1,16 @@
+package monster.minions.binocularss.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import monster.minions.binocularss.R
+
+class BookmarksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmarks)
+
+
+    }
+}
