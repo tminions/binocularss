cedc9645b6d83ecf5b574ddeed6ace1624d485e8
hisbaan <hisbaan@gmail.com>
2021-11-06 20:07:42 -0400

    Add duplicate feed checking system (before DB duplicate checking

--- app/src/main/java/monster/minions/binocularss/activities/AddFeedActivity.kt
+++ app/src/main/java/monster/minions/binocularss/activities/AddFeedActivity.kt
@@ -1,7 +1,6 @@
 package monster.minions.binocularss.activities
 
 import android.os.Bundle
-import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -48,10 +47,6 @@ class AddFeedActivity : ComponentActivity() {
 
         GlobalScope.launch(Dispatchers.Main) {
             MainActivity.feedGroup = viewModel.pullRss(MainActivity.feedGroup, parser)
-            // Debug toast to confirm that the data has come back to the MainActivity variable.
-            // for (feed in MainActivity.feedGroup.feeds) {
-            //     Toast.makeText(context, "Pulled: ${feed.title}", Toast.LENGTH_SHORT).show()
-            // }
 
             var text = ""
             println(MainActivity.feedGroup.feeds.size)
@@ -60,10 +55,6 @@ class AddFeedActivity : ComponentActivity() {
                 text += "\n"
             }
             MainActivity.feedGroupText.value = text
-
-            Log.d("AddFeed: updateRss", (MainActivity.feedGroup.feeds[0].articles[0].description).toString())
-            // TODO try and save to DB here or when returning back to main
-            //  or figure out what is causing the other feeds to be wiped out
         }
     }
 
@@ -80,7 +71,7 @@ class AddFeedActivity : ComponentActivity() {
         }
     }
 
-    var text = mutableStateOf("")
+    private var text = mutableStateOf("")
 
     @Composable
     fun FeedTextField() {
@@ -105,14 +96,27 @@ class AddFeedActivity : ComponentActivity() {
                 println(url)
 
                 if (Patterns.WEB_URL.matcher(url).matches()) {
-                    // TODO this should be adding one feed to a list of feeds but it seems like
-                    //  it's adding the one feed to an empty list? Maybe when MainActivity is stopped
-                    //  the companion variables are deleted?
-                    MainActivity.feedGroup.feeds.add(Feed(link = url))
 
-                    // Update the RSS feed
-                    updateRss(MainActivity.parser)
-                    finish()
+                    // Check if the feed is already in the feedGroup
+                    val feedToAdd = Feed(source = url)
+                    var inFeedGroup = false
+                    for (feed in MainActivity.feedGroup.feeds) {
+                        if (feedToAdd == feed) {
+                            inFeedGroup = true
+                            Toast.makeText(this@AddFeedActivity,
+                                "You've already added that RSS feed",
+                                Toast.LENGTH_SHORT).show()
+                            continue
+                        }
+                    }
+
+                    // Add feed and update feeds if the feed is not in the feedGroup
+                    if (!inFeedGroup) {
+                        MainActivity.feedGroup.feeds.add(Feed(source = url))
+                        updateRss(MainActivity.parser)
+                        finish()
+                    }
+
                 } else {
                     Toast.makeText(this@AddFeedActivity, "Invalid URL", Toast.LENGTH_SHORT).show()
                 }
