400c01e921c39422e83a9eae31a28cf44b850409
Ismail Ahmed <isahmed0149@gmail.com>
2021-11-10 08:21:00 -0500

    Added image to bookmark, fix allignment with title

--- app/src/main/java/monster/minions/binocularss/activities/BookmarksActivity.kt
+++ app/src/main/java/monster/minions/binocularss/activities/BookmarksActivity.kt
@@ -1,7 +1,10 @@
 package monster.minions.binocularss.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
@@ -10,22 +13,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import monster.minions.binocularss.R
 import monster.minions.binocularss.dataclasses.Article
-import monster.minions.binocularss.dataclasses.DummyData
 import monster.minions.binocularss.ui.theme.BinoculaRSSTheme
+import kotlin.math.max
 
 class BookmarksActivity : AppCompatActivity() {
+    @ExperimentalMaterialApi
     @ExperimentalCoilApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +61,7 @@ class BookmarksActivity : AppCompatActivity() {
      * @param bookmarked_articles MutableList of articles that are bookmarked
      * across all feeds
      */
+    @ExperimentalMaterialApi
     @ExperimentalCoilApi
     @Composable
     fun Bookmarks(bookmarked_articles: MutableList<Article>){
@@ -68,12 +79,18 @@ class BookmarksActivity : AppCompatActivity() {
      *
      * @param article
      */
+    @ExperimentalMaterialApi
     @ExperimentalCoilApi
     @Composable
     fun Bookmark(article: Article){
 
         Card(
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+            onClick = {
+                val uri = Uri.parse(article.link)
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
         ) {
             CardContent(
                 article
@@ -87,7 +104,7 @@ class BookmarksActivity : AppCompatActivity() {
     @Composable
     fun CardContent(article: Article){
 
-        //var expanded by remember { mutableStateOf(false) }
+        var expanded by remember { mutableStateOf(true) }
 
         Row(
             modifier = Modifier
@@ -98,35 +115,56 @@ class BookmarksActivity : AppCompatActivity() {
                         stiffness = Spring.StiffnessLow
                     )
                 )
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Bottom,
+
         ) {
+            var maxBaseline by remember { mutableStateOf(0f) }
+            fun updateMaxBaseline(textLayoutResult: TextLayoutResult){
+                maxBaseline = max(maxBaseline, textLayoutResult.size.height - textLayoutResult.lastBaseline)
+            }
+            val topBaselinePadding = with(LocalDensity.current) { maxBaseline.toDp() }
+
+
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(12.dp)
             ) {
-                Image(
-                    painter = rememberImagePainter(
-                        data = if (article.image != null) article.image else "https://miro.medium.com/max/500/0*-ouKIOsDCzVCTjK-.png",
-                        builder = {
-                            placeholder(R.drawable.ic_launcher_foreground)
-                        }
-                    ),
-                    contentDescription = article.description,
-                    modifier = Modifier
-                        .width(10.dp)
-                        .height(10.dp)
-                )
+                Row(
 
-
+                ) {
+                    Text(
+                        text = article.title.toString(),
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier.width(120.dp)
+                            .paddingFromBaseline(bottom = topBaselinePadding),
+                        onTextLayout = ::updateMaxBaseline
+                    )
+                    Image(
+                        painter = rememberImagePainter(
+                            data = if (article.image != null) article.image else "https://miro.medium.com/max/500/0*-ouKIOsDCzVCTjK-.png",
+                            builder = {
+                                placeholder(R.drawable.ic_launcher_foreground)
+                            }
+                        ),
+                        contentDescription = article.description,
+                        modifier = Modifier
+                            .padding(bottom = topBaselinePadding)
+                            .size(120.dp)
+                    )
+                }
+                if (expanded){
+                   Text(
+                       text = article.link.toString()
+                   )
+                }
             }
-            Text(
-                text = article.title.toString(),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                textAlign = TextAlign.Right,
-            )
-        }
 
+
+
+
+        }
 
     }
 
@@ -145,6 +183,9 @@ class BookmarksActivity : AppCompatActivity() {
             articles = feed.articles
             for (article in articles){
                 if (article.bookmarked){
+                    if (article.image == null && article.title.toString().startsWith("Covid-19 spreads through the air.")){
+                        Log.d("NULL IMAGE", "No image for this article")
+                    }
                     bookmarkedArticles.add(article)
                 }
             }
