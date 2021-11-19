8c6bdef1a26429781c54721bd0e8a9caed1a14f9
Ismail Ahmed <isahmed0149@gmail.com>
2021-11-19 09:40:02 -0500

    Fix Fuzzy Searching

--- app/src/main/java/monster/minions/binocularss/operations/ArticleSearchComparator.kt
+++ app/src/main/java/monster/minions/binocularss/operations/ArticleSearchComparator.kt
@@ -1,5 +1,6 @@
 package monster.minions.binocularss.operations
 
+import android.util.Log
 import me.xdrop.fuzzywuzzy.FuzzySearch
 import monster.minions.binocularss.dataclasses.Article
 
@@ -11,18 +12,21 @@ class ArticleSearchComparator(
 
     /**
      * Compare both articles using FuzzySearch ratio to determine which is a better
-     * match to a user's query. Return a negative int, it p1 is a better match, 0 if
-     * they are an equal match, and a positive int if p0 is a better match
+     * match to a user's query. Return a negative int, it p0 is a better match, 0 if
+     * they are an equal match, and a positive int if p1 is a better match
      *
      * @param p0 The first article to compare
      * @param p1 The second article to compare
      */
     override fun compare(p0: Article?, p1: Article?): Int {
 
-       val p0Ratio = FuzzySearch.tokenSetRatio(query, p0?.title)
-       val p1Ratio = FuzzySearch.tokenSetRatio(query, p1?.title)
 
-       return p0Ratio - p1Ratio
+       val p0Ratio = FuzzySearch.tokenSetRatio(query, p0?.title.toString())
+       val p1Ratio = FuzzySearch.tokenSetRatio(query, p1?.title.toString())
+
+       // Do p1Ration - p0Ratio because when we fill up the lazy column, we want
+       // the articles with higher ratios to show up first and the list is in ascending order
+       return p1Ratio - p0Ratio
     }
 
 
