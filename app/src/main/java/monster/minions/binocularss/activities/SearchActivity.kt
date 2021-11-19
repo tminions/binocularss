91af3f9badaac2f2206a35f7d5a5d8c47978d179
Ismail Ahmed <isahmed0149@gmail.com>
2021-11-19 12:15:54 -0500

    Add imports and remove SearchIcon import

--- app/src/main/java/monster/minions/binocularss/activities/SearchActivity.kt
+++ app/src/main/java/monster/minions/binocularss/activities/SearchActivity.kt
@@ -1,15 +1,21 @@
 package monster.minions.binocularss.activities
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +31,6 @@ import monster.minions.binocularss.operations.ArticleSearchComparator
 import monster.minions.binocularss.room.AppDatabase
 import monster.minions.binocularss.room.FeedDao
 import monster.minions.binocularss.ui.ArticleCard
-import monster.minions.binocularss.ui.SearchIcon
 
 class SearchActivity : ComponentActivity() {
 
