e2d296e49474331caeb1cfd94a0318142423e317
Simon Chen <simonchen.sc.2002@gmail.com>
2021-12-05 18:07:33 -0500

    Fix URL trim test

--- app/src/test/java/monster/minions/binocularss/UrlTrimTest.kt
+++ app/src/test/java/monster/minions/binocularss/UrlTrimTest.kt
@@ -1,12 +1,9 @@
 package monster.minions.binocularss
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import monster.minions.binocularss.activities.AddFeedActivity
 import monster.minions.binocularss.operations.trimWhitespace
 import org.junit.Test
 
 import org.junit.Assert.*
-import org.junit.runner.RunWith
 
 /**
  * Test to see if URL trimming works properly.
