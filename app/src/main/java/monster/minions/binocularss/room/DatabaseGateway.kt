5cfef8a0c299f516508e0b2dadc70120df0ab601
Ismail Ahmed <isahmed0149@gmail.com>
2021-12-04 20:07:07 -0500

    Implement read and write methods

--- app/src/main/java/monster/minions/binocularss/room/DatabaseGateway.kt
+++ app/src/main/java/monster/minions/binocularss/room/DatabaseGateway.kt
@@ -1,23 +1,25 @@
 package monster.minions.binocularss.room
 
 import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import monster.minions.binocularss.dataclasses.Feed
 
 
 class DatabaseGateway {
 
-    private lateinit var db: RoomDatabase
-    private lateinit var feedDao: FeedDao
+    private var db: RoomDatabase = Room
+        .databaseBuilder(context, AppDatabase::class.java, "feed-db")
+        .allowMainThreadQueries()
+        .build()
+    private var feedDao: FeedDao = (db as AppDatabase).feedDao()
     private lateinit var context: Context
 
-    public fun read(){
-
-
+    fun read(): MutableList<Feed>{
+        return feedDao.getAll()
     }
 
-    public fun write(feedsToAdd: MutableList<Feed>){
+    fun write(feedsToAdd: MutableList<Feed>){
         feedDao.insertAll(*(feedsToAdd.toTypedArray()))
-
     }
 }
