46527b1ec2952eb0458a501161e35fb7beec37dc
Ismail Ahmed <isahmed0149@gmail.com>
2021-12-05 14:32:08 -0500

    Add more comments to DatabaseGateway

--- app/src/main/java/monster/minions/binocularss/room/DatabaseGateway.kt
+++ app/src/main/java/monster/minions/binocularss/room/DatabaseGateway.kt
@@ -8,6 +8,12 @@ import monster.minions.binocularss.dataclasses.Feed
 
 class DatabaseGateway(private var context: Context) {
 
+    /**
+     * This class is meant acts as an interface between
+     * the UI and the database in order to reduce coupling within
+     * the outermost layer
+     */
+
     private lateinit var db: RoomDatabase
     private lateinit var feedDao: FeedDao
 
@@ -23,7 +29,10 @@ class DatabaseGateway(private var context: Context) {
         return feedDao.getAll()
     }
 
-
+    /**
+     * Initialize the database, make this method private since database
+     * should only need to be set once in the initializer.
+     */
     private fun setDb(){
         db = Room
             .databaseBuilder(context, AppDatabase::class.java, "feed-db")
@@ -32,6 +41,9 @@ class DatabaseGateway(private var context: Context) {
 
     }
 
+    /**
+     * Initialize the Data Access Object for Feeds
+      */
     private fun setFeedDao(){
         feedDao = (db as AppDatabase).feedDao()
     }
